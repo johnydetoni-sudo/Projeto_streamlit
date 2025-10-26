2a62af6ee51dc17bfef84310a8ac67d7a64601c4
johnydetoni-sudo <johnydetoni@gmail.com>
2025-10-26 00:04:51 -0300

    Arquivos enviados

---
+++ Cadastro.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+from datetime import date
+
+def gravar_dados(nome, dt_nasc, tipo):
+    if nome and dt_nasc <= date.today():
+        with open("clientes.csv", "a", encoding="utf-8") as file:
+            file.write(f"{nome},{dt_nasc},{tipo}\n")
+        st.session_state["sucesso"] = True
+    else:
+        st.session_state["sucesso"] = False
+
+st.set_page_config(
+    page_title="Cadastro de clientes",
+    page_icon="📖",
+)
+
+st.title("📖 Cadastro de clientes")
+st.divider()
+
+nome = st.text_input("Digite o nome do cliente:",
+                     key="nome_cliente")
+
+dt_nasc = st.date_input("Data de nascimento:",
+                        format="DD/MM/YYYY",
+                        key="data_nascimento")
+
+tipo = st.selectbox("Tipo de cliente:",
+                    ["Pessoa Física", "Pessoa Jurídica"])
+
+btn_cadastrar = st.button("Cadastrar cliente",
+                          on_click=gravar_dados,
+                          args=[nome, dt_nasc, tipo])
+
+if btn_cadastrar:
+    if st.session_state["sucesso"]:
+        st.success("Cliente cadastrado com sucesso!",
+                   icon="✅")
+    else:
+        st.error("Erro ao cadastrar cliente. Verifique os dados informados.",
+                 icon="❌")
+
